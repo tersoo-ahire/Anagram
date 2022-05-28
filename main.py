bd9a38cad7453742dfd9ff5fd4a7ef5e4519c68f
Ahire Tersoo <105167338+tersoo-ahire@users.noreply.github.com>
2022-05-29 00:05:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@
 def find_anagram():
     str1 = input("string1:")
     str2 = input("string2:")
-    sorted_str1 = sorted(str1)
-    sorted_str2 =sorted(str2)
 
     if len(str1)==len(str2):
         if sorted(str1) == sorted(str2):
